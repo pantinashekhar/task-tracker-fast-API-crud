487832cf84d77644220b188d9dd65217ca3ebd54
Chandrashekhar Pantina <pantinashekhar87@gmail.com>
2026-01-01 05:16:35 +0530

    added PUT,PATCH and DELETE

--- main.py
+++ main.py
@@ -55,6 +55,36 @@ def create_task(task: Task,conn=Depends(get_db_connection)):
     conn.close()
     return task
 
+@app.put("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: int, task: Task,conn=Depends(get_db_connection)):
+    cursor = conn.cursor()
+    cursor.execute("UPDATE tasks SET title = %s, description = %s, status = %s, due_date = %s WHERE id = %s",
+                (task.title, task.description, task.status, task.due_date, task_id))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return task
+
+@app.patch("/tasks/{task_id}/{status}", response_model=Task)
+def update_task_status(task:Task,task_id: int, status: str,conn=Depends(get_db_connection)):
+    cursor = conn.cursor()
+    cursor.execute("UPDATE tasks SET status = %s WHERE id = %s", (status, task_id))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return task
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int,conn=Depends(get_db_connection)):
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM tasks WHERE id = %s", (task_id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return {"message": "Task deleted successfully"}
+
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1")
